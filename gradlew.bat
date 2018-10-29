9762c05a27ca2654c8249009255fa07d2e4da18e
Sujin Kim <gfsusan@naver.com>
2018-10-29 17:20:04 +0900

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,49 @@
+<application>
+  <component name="RecentProjectsManager">
+    <option name="recentPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/android-3D-model-viewer-master" />
+        <option value="$USER_HOME$/Documents/OpenGLPractice" />
+        <option value="$USER_HOME$/Documents/android-ndk-master" />
+        <option value="$USER_HOME$/Documents/achi/Android" />
+      </list>
+    </option>
+    <option name="openPaths">
+      <list>
+        <option value="$USER_HOME$/Desktop/android-3D-model-viewer-master" />
+      </list>
+    </option>
+    <option name="lastPath" value="$USER_HOME$/Desktop/android-3D-model-viewer-master" />
+    <option name="pid" value="" />
+    <option name="additionalInfo">
+      <map>
+        <entry key="$USER_HOME$/Desktop/android-3D-model-viewer-master">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5014246" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1538225324132" />
+              <option name="buildTimestamp" value="1537211825442" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="$USER_HOME$/Documents/OpenGLPractice">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5014246" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1538208927685" />
+              <option name="buildTimestamp" value="1537211825442" />
+            </RecentProjectMetaInfo>
+          </value>
+        </entry>
+        <entry key="$USER_HOME$/Documents/achi/Android">
+          <value>
+            <RecentProjectMetaInfo>
+              <option name="build" value="AI-181.5540.7.32.5014246" />
+              <option name="productionCode" value="AI" />
+              <option name="binFolder" value="$APPLICATION_HOME_DIR$/bin" />
+              <option name="projectOpenTimestamp" value="1538117788931" />
+              <option name="buildTimestamp" value="15372118
